5e3e1adce762ed9ad7761efcecced6f68f6160ad
alfdagos <alfredo.dagostino@gmail.com>
2022-08-03 11:01:14 +0200

    Aggiunta parametro letto da configurazione

--- src/main/java/it/alf/testk8s/controller/HelloWorldController.java
+++ src/main/java/it/alf/testk8s/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package it.alf.testk8s.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Value("${name}")
+    private String name;
+
     @RequestMapping("/")
     public String hello() {
-        return "Hello Alf";
+        return "Hello "+name;
     }
 }  
